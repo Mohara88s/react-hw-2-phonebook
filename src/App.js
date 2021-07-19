31bf6c799b5fca4b9204a8d2f6a2f723ca0efcc6
Mohara88s <73835861+Mohara88s@users.noreply.github.com>
2021-07-19 08:55:35 +0300

    step 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 class App extends Component {
   state = {
@@ -6,24 +7,50 @@ class App extends Component {
     name: '',
   };
   hendelInputChange = event => {
-    console.log(event.currentTarget.value);
     this.setState({ [event.currentTarget.name]: event.currentTarget.value });
   };
+  hendelSubmit = event => {
+    event.preventDefault();
+
+    const newContact = {
+      id: uuidv4(),
+      name: this.state.name,
+    };
+
+    this.setState(prevState => ({
+      contacts: [newContact, ...prevState.contacts],
+    }));
+  };
+
   render() {
+    const contactsList = this.state.contacts;
     return (
-      <form>
-        <label>
-          Name
-          <input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
-            required
-            onChange={this.hendelInputChange}
-          />
-        </label>
-      </form>
+      <>
+        <form onSubmit={this.hendelSubmit}>
+          <h2>Phonebook</h2>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+              required
+              onChange={this.hendelInputChange}
+            />
+          </label>
+          <button type="submit">Save</button>
+        </form>
+
+        <h2>Contacts</h2>
+        <ul>
+          {contactsList.map(({ id, name }) => (
+            <li key={id}>
+              <p>{name}</p>
+            </li>
+          ))}
+        </ul>
+      </>
     );
   }
 }
